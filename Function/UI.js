a691ed68e8e8ce462db8f82bd6c468dd632d4e17
shihao <sh1009@163.com>
2017-08-08 09:53:54 +0800

    增加警告

--- Function/UI.js
+++ Function/UI.js
@@ -9,26 +9,47 @@ function ShowLayer(id){
 }
 
 function HideButton(str){
-    var sonObjArray = new Array();
+    var ObjArray = new Array();
     var i = 0;
     while (str.search(/,/)!==-1) {
-      sonObjArray[i] = ucctojs(str.slice(0,DividePoint(str,',')));
-      sonObjArray[i].style.display="none";
+      ObjArray[i] = ucctojs(str.slice(0,DividePoint(str,',')));
+      if(ObjArray[i].getAttribute("ucctype")=="Image"||ObjArray[i].getAttribute("ucctype")=="Gif"||ObjArray[i].getAttribute("ucctype")=="Apng"||ObjArray[i].getAttribute("ucctype")=="Label"){
+         ObjArray[i].style.display="none";
+      }
+      else{
+          console.log('error:'+ str.slice(0,DividePoint(str,','))+'操作对象不是button类型，此函数对其无作用');
+      }
       i++;
       str = str.slice(DividePoint(str,',')+1,str.length);
+
     }
-    sonObjArray[i] = ucctojs(str);
-    sonObjArray[i].style.display="none";
+    ObjArray[i] = ucctojs(str);
+    if(ObjArray[i].getAttribute("ucctype")=="Image"||ObjArray[i].getAttribute("ucctype")=="Gif"||ObjArray[i].getAttribute("ucctype")=="Apng"||ObjArray[i].getAttribute("ucctype")=="Label"){
+       ObjArray[i].style.display="none";
+    }
+    else{
+        console.log('error:'+ str+'操作对象不是button类型，此函数对其无作用');
+    }
 }
 function showButton(str){
-    var sonObjArray = new Array();
+    var ObjArray = new Array();
     var i = 0;
     while (str.search(/,/)!==-1) {
-      sonObjArray[i] = ucctojs(str.slice(0,DividePoint(str,',')));
-      sonObjArray[i].style.display="block";
+      ObjArray[i] = ucctojs(str.slice(0,DividePoint(str,',')));
+      if(ObjArray[i].getAttribute("ucctype")=="Image"||ObjArray[i].getAttribute("ucctype")=="Gif"||ObjArray[i].getAttribute("ucctype")=="Apng"||ObjArray[i].getAttribute("ucctype")=="Label"){
+         ObjArray[i].style.display="block";
+      }
+      else{
+          console.log('error:'+ str.slice(0,DividePoint(str,','))+'操作对象不是button类型，此函数对其无作用');
+      }
       i++;
       str = str.slice(DividePoint(str,',')+1,str.length);
     }
-    sonObjArray[i] = ucctojs(str);
-    sonObjArray[i].style.display="block";
+    ObjArray[i] = ucctojs(str);
+    if(ObjArray[i].getAttribute("ucctype")=="Image"||ObjArray[i].getAttribute("ucctype")=="Gif"||ObjArray[i].getAttribute("ucctype")=="Apng"||ObjArray[i].getAttribute("ucctype")=="Label"){
+       ObjArray[i].style.display="block";
+    }
+    else{
+        console.log('error:'+ str+'操作对象不是button类型，此函数对其无作用');
+    }
 }
