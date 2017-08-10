958fe7f1399ebd1df6b0225dddd5d293d50ec7fc
shihao <sh1009@163.com>
2017-08-10 20:15:36 +0800

    加了日志

--- Function/UI.js
+++ Function/UI.js
@@ -10,31 +10,33 @@ function ShowLayer(id){
 
 function HideButton(Str){
     Str +=',';
-    var Obj;
+    var Obj,Objspan;
     while (DividePoint(Str,',')!==-1) {
       Obj = ucctojs(Str.slice(0,DividePoint(Str,',')));
+      Objspan = ucctojs(Str.slice(0,DividePoint(Str,','))+"span");
       if(Obj.getAttribute("ucctype")=="Image"||Obj.getAttribute("ucctype")=="Gif"||Obj.getAttribute("ucctype")=="Apng"||Obj.getAttribute("ucctype")=="Label"){
          Obj.style.display="none";
+         Objspan.style.display="none";
       }
       else{
           console.log('error:'+ Str.slice(0,DividePoint(Str,','))+'操作对象不是button类型，此函数对其无作用');
       }
-      i++;
-      Str = Str.slice(DividePoint(Str,',')+1,str.length);
+      Str = Str.slice(DividePoint(Str,',')+1,Str.length);
     }
 }
-function showButton(str){
+function ShowButton(Str){
     Str +=',';
-    var Obj;
+    var Obj,Objspan;
     while (DividePoint(Str,',')!==-1) {
       Obj = ucctojs(Str.slice(0,DividePoint(Str,',')));
+      Objspan = ucctojs(Str.slice(0,DividePoint(Str,','))+"span");
       if(Obj.getAttribute("ucctype")=="Image"||Obj.getAttribute("ucctype")=="Gif"||Obj.getAttribute("ucctype")=="Apng"||Obj.getAttribute("ucctype")=="Label"){
          Obj.style.display="block";
+         Objspan.style.display="block";
       }
       else{
           console.log('error:'+ Str.slice(0,DividePoint(Str,','))+'操作对象不是button类型，此函数对其无作用');
       }
-      i++;
-      Str = Str.slice(DividePoint(Str,',')+1,str.length);
+      Str = Str.slice(DividePoint(Str,',')+1,Str.length);
     }
 }
